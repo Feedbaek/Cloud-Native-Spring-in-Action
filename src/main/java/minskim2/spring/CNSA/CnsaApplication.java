e937e9b6114ba92b97ece5e41f3d59c589f3ee67
Feedbaek <nicek789@naver.com>
2024-08-09 17:56:01 +0900

    Docs: Chapter 1 클라우드 네이티브 소개 작성중

---
+++ src/main/java/minskim2/spring/CNSA/CnsaApplication.java
@@ -0,0 +1,13 @@
+package minskim2.spring.CNSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minskim2/spring/CNSA/CnsaApplicationTests.java
@@ -0,0 +1,13 @@
+package minskim2.spring.CNSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
